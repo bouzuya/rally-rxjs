a13a163a6a2398e0735bcb873f63865e1df85706
bouzuya <m@bouzuya.net>
2016-02-21 19:32:05 +0900

    remove old initializers

--- src/app/initializers/list-user.ts
+++
@@ -1,8 +0,0 @@
-import { State } from '../models/state';
-import { User } from '../models/user';
-
-export default function listUserInitializer(): Promise<State> {
-  return User
-    .fetchUsers()
-    .then(users => ({ users, user: null }));
-};

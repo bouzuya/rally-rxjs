c9acd1c748aac3649502c8981b881f580b1c12ab
bouzuya <m@bouzuya.net>
2016-02-22 22:30:30 +0900

    add route to /stamp_rallies/:id

--- src/app/configs/routes.ts
+++ src/app/configs/routes.ts
@@ -2,7 +2,8 @@ import { Route } from '../../framework/router';
 
 const routes: Route[] = [
   { path: '/sign_in', name: 'sign_in#index' },
-  { path: '/stamp_rallies', name: 'stamp_rallies#index' }
+  { path: '/stamp_rallies', name: 'stamp_rallies#index' },
+  { path: '/stamp_rallies/:id', name: 'stamp_rallies#show' }
 ];
 
 export { routes };

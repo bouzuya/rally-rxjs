48704213a57044e83a21282bcc93f717f152aa95
bouzuya <m@bouzuya.net>
2016-02-22 23:41:20 +0900

    update token fetch-stamp-rally-list for RequestStampRallyShowAction

--- src/app/properties/token/fetch-stamp-rally-list.ts
+++ src/app/properties/token/fetch-stamp-rally-list.ts
@@ -5,16 +5,36 @@ import { Action } from '../../models/action';
 import { Updater } from '../../models/updater';
 
 import { Token } from '../../models/token';
-import { create } from '../../actions/request-stamp-rally-index';
-import { is } from '../../actions/go-to-stamp-rally-list';
+import {
+  create as createStampRallyList
+} from '../../actions/request-stamp-rally-index';
+import {
+  is as isStampRallyList
+} from '../../actions/go-to-stamp-rally-list';
+import {
+  create as createStampRallyShow
+} from '../../actions/request-stamp-rally-show';
+import {
+  is as isStampRallyShow
+} from '../../actions/go-to-stamp-rally-show';
 
 export default function updater$(
   action$: Observable<Action<any>>,
   reaction: (action: Action<any>) => void
 ): Observable<Updater<Token>> {
-  return action$
-    .filter(is)
-    .map(() => (token: Token) => {
+  const create$: Observable<(...args: any[]) => Action<any>> = Observable
+    .merge(
+      action$
+        .filter(isStampRallyList)
+        .map(() => createStampRallyList),
+      action$
+        .filter(isStampRallyShow)
+        .map(({ params: id }) => ({ token }: Token) => {
+          return createStampRallyShow(token, id);
+        })
+    );
+  return create$
+    .map(create => (token: Token) => {
       reaction(create(token));
       return token;
     });

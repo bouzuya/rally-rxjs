6ac963a4af2529875679a9ab65bbc4b247655bb3
bouzuya <m@bouzuya.net>
2016-02-23 21:39:24 +0900

    add spot list to stamp rally view

--- src/app/views/stamp-rally-show-page.ts
+++ src/app/views/stamp-rally-show-page.ts
@@ -4,8 +4,17 @@ import { State } from '../models/state';
 
 export default function render(state: State): VTree {
   return h('div.stamp-rally-show-page', [
-    state.stampRally
-    ? h('h1', [state.stampRally.name])
-    : h('h1', ['loading...'])
+    (
+      state.stampRally
+      ? h('h1', [state.stampRally.name])
+      : h('h1', ['loading...'])
+    ),
+    h('ul', state.spots.map(spot => {
+      return h('li', [
+        spot.id,
+        spot.name,
+        spot.tagline
+      ]);
+    }))
   ]);
 }

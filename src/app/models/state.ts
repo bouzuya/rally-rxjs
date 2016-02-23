cb3930d3a0083ac595aee54885c5cc4ba5bca92e
bouzuya <m@bouzuya.net>
2016-02-23 21:35:18 +0900

    add spots to State

--- src/app/models/state.ts
+++ src/app/models/state.ts
@@ -1,10 +1,12 @@
 import { SignIn } from '../models/sign-in';
+import { Spot } from '../models/spot';
 import { StampRally } from '../models/stamp-rally';
 import { Token } from '../models/token';
 
 type State = {
   currentPage: string;
   signIn: SignIn;
+  spots: Spot[];
   stampRallies: StampRally[];
   stampRally: StampRally;
   token: Token;

bcef7e484867b3ba25d19698f344b1833f2ffd35
bouzuya <m@bouzuya.net>
2016-02-25 19:54:47 +0900

    reset form after spot created

--- src/app/properties/spot-form/change-name.ts
+++ src/app/properties/spot-form/change-name.ts
@@ -3,15 +3,21 @@ import { Observable } from 'rxjs';
 import { Action } from '../../models/action';
 import { Updater } from '../../models/updater';
 
-import { is } from '../../actions/change-spot-form-name';
+import { is as isChangeName } from '../../actions/change-spot-form-name';
+import { is as isResponseSpotCreate } from '../../actions/response-spot-create';
 import { SpotForm } from '../../models/spot-form';
 
 export default function updater$(
   action$: Observable<Action<any>>
 ): Observable<Updater<SpotForm>> {
-  return action$
-    .filter(is)
-    .map(({ params: { value } }) => (state: SpotForm) => {
-      return Object.assign({}, state, { name: value });
-    });
+  return Observable
+    .merge(
+      action$
+        .filter(isChangeName)
+        .map(({ params: { value } }) => value),
+      action$
+        .filter(isResponseSpotCreate)
+        .map(() => null)
+    )
+    .map(name => (state: SpotForm) => Object.assign({}, state, { name }));
 }

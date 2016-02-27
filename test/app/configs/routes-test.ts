3d0794ae25e8776f6a5b72862ce4ffad329c5332
bouzuya <m@bouzuya.net>
2016-02-27 10:11:42 +0900

    fix app/config/routes

---
+++ test/app/configs/routes-test.ts
@@ -0,0 +1,36 @@
+import assert from 'power-assert';
+import { routes } from '../../../src/app/configs/routes';
+import { Router, Route, RouteAction } from '../../../src/framework/router';
+
+describe('app/config/routes', function() {
+  beforeEach(function() {
+    this.router = new Router(routes);
+  });
+
+  context('sign_in#index', function() {
+    it('works', function() {
+      const path = '/sign_in';
+      const match = this.router.routes(path);
+      assert(match.type === 'sign_in#index');
+      assert.deepEqual(match.params, {});
+    });
+  });
+
+  context('stamp_rallies#index', function() {
+    it('works', function() {
+      const path = '/stamp_rallies';
+      const match = this.router.routes(path);
+      assert(match.type === 'stamp_rallies#index');
+      assert.deepEqual(match.params, {});
+    });
+  });
+
+  context('stamp_rallies#show', function() {
+    it('works', function() {
+      const path = '/stamp_rallies/bouzuya';
+      const match = this.router.routes(path);
+      assert(match.type === 'stamp_rallies#show');
+      assert.deepEqual(match.params, { id: 'bouzuya' });
+    });
+  });
+});

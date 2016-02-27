26ec0ca0e585489bae2aab19f052809d3c9cb05b
bouzuya <m@bouzuya.net>
2016-02-27 23:03:30 +0900

    add style

--- src/app/views/all.ts
+++ src/app/views/all.ts
@@ -8,6 +8,7 @@ export default function render(state: State): VTree {
   return h('html', [
     h('head', [
       h('title', ['rally-rxjs']),
+      h('link', { rel: 'stylesheet', href: '/styles/index.css' }, []),
       h('script', ['var INITIAL_STATE = ' + htmlescape(state) + ';'])
     ]),
     h('body', [

d01d555972c643ee345ba8ce8fdda6e5b9632c16
bouzuya <m@bouzuya.net>
2016-03-05 23:09:12 +0900

    add makeHTTPResponse

---
+++ src/app/http-response.ts
@@ -0,0 +1,42 @@
+import { O, A } from 'b-o-a';
+
+import { create as httpResponse } from './actions/http-response';
+import { from as httpRequest$ } from './actions/http-request';
+import signInIndexInitializer from './initializers/sign-in-index';
+import stampRalliesIndexInitializer from './initializers/stamp-rallies-index';
+import stampRalliesShowInitializer from './initializers/stamp-rallies-show';
+
+export default function makeHTTPResponse(action$: O<A<any>>): O<A<any>> {
+  return O
+    .merge(
+      httpRequest$(action$)
+        .filter(({ route: { name } }) => name === 'sign_in#index')
+        .map(({ params, http }) => {
+          return signInIndexInitializer() // no params
+            .then(
+              state => ({ state, http }),
+              error => ({ error, http })
+            );
+        }),
+      httpRequest$(action$)
+        .filter(({ route: { name } }) => name === 'stamp_rallies#index')
+        .map(({ params, http }) => {
+          return stampRalliesIndexInitializer() // no params
+            .then(
+              state => ({ state, http }),
+              error => ({ error, http })
+            );
+        }),
+      httpRequest$(action$)
+        .filter(({ route: { name } }) => name === 'stamp_rallies#show')
+        .map(({ params, http }) => {
+          return stampRalliesShowInitializer(params)
+            .then(
+              state => ({ state, http }),
+              error => ({ error, http })
+            );
+        })
+    )
+    .mergeMap((promise: Promise<any>) => O.fromPromise(promise))
+    .map(httpResponse);
+}

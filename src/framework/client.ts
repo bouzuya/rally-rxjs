2a791c636bbcbda199fd4344bdeb1f3bc1666816
bouzuya <m@bouzuya.net>
2016-03-05 10:50:11 +0900

    refactor

--- src/framework/client.ts
+++ src/framework/client.ts
@@ -1,14 +1,13 @@
 import { A, O, App, run } from 'b-o-a';
-import { Subject } from 'rxjs';
 import { Executor } from './executor';
 
 type ClientApp = (action$: O<A<any>>, options: any) => O<A<any>>;
 
-const makeApp = (clientApp: ClientApp, executors: Executor[]): App => {
-  const app = (
-    action$: O<A<any>>,
-    { re }: { re: (action: A<any>) => void; }
-  ): O<A<any>> => {
+export default function runClient(
+  clientApp: ClientApp,
+  executors: Executor[]
+): void {
+  run((action$, { re }) => {
     const options = executors.reduce((c, { before }) => before(c), { re });
     const a$: O<A<any>> = executors
       .reduce((a$, { execute }) => a$.map(execute(options)), action$)
@@ -16,13 +15,5 @@ const makeApp = (clientApp: ClientApp, executors: Executor[]): App => {
     const app$ = clientApp(a$, options);
     executors.reduce((c, { after }) => after(c), options);
     return app$;
-  };
-  return app;
-};
-
-export default function main(
-  clientApp: ClientApp,
-  executors: Executor[]
-): void {
-  run(makeApp(clientApp, executors));
+  });
 }

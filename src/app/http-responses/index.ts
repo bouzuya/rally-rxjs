18d56ea45d1a19f0528092d82b8fdd59e05c6f17
bouzuya <m@bouzuya.net>
2016-03-08 20:30:33 +0900

    refactor

--- src/app/http-responses/index.ts
+++ src/app/http-responses/index.ts
@@ -10,37 +10,20 @@ import signInIndex from '../http-responses/sign-in-index';
 import stampRalliesIndex from '../http-responses/stamp-rallies-index';
 import stampRalliesShow from '../http-responses/stamp-rallies-show';
 
+const inits = {
+  'sign_in#index': signInIndex,
+  'stamp_rallies#index': stampRalliesIndex,
+  'stamp_rallies#show': stampRalliesShow
+};
+
 export default function makeHTTPResponse(action$: O<A<any>>): O<A<any>> {
-  return O
-    .merge(
-      httpRequest$(action$)
-        .filter(({ route: { name } }) => name === 'sign_in#index')
-        .map(({ params, http }) => {
-          return signInIndex(params)
-            .then(
-              state => ({ state, http }),
-              error => ({ error, http })
-            );
-        }),
-      httpRequest$(action$)
-        .filter(({ route: { name } }) => name === 'stamp_rallies#index')
-        .map(({ params, http }) => {
-          return stampRalliesIndex(params)
-            .then(
-              state => ({ state, http }),
-              error => ({ error, http })
-            );
-        }),
-      httpRequest$(action$)
-        .filter(({ route: { name } }) => name === 'stamp_rallies#show')
-        .map(({ params, http }) => {
-          return stampRalliesShow(params)
-            .then(
-              state => ({ state, http }),
-              error => ({ error, http })
-            );
-        })
-    )
+  return httpRequest$(action$)
+    .map(({ route: { name }, params, http }) => {
+      return inits[name](params).then(
+        state => ({ state, http }),
+        error => ({ error, http })
+      );
+    })
     .mergeMap((promise: Promise<any>) => O.fromPromise(promise))
     .map(httpResponse);
 }

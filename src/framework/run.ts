80aacbc3ce06c6947ae9838a56a0da4185755a3f
bouzuya <m@bouzuya.net>
2016-03-08 15:20:18 +0900

    fix run api

--- src/framework/run.ts
+++ src/framework/run.ts
@@ -1,10 +1,10 @@
 import { A, O, App, run } from 'b-o-a';
 import { Executor } from './executor';
 
-type ClientApp = (action$: O<A<any>>, options: any) => O<A<any>>;
+type UserApp = (action$: O<A<any>>, options: any) => O<A<any>>;
 
-export default function runClient(
-  clientApp: ClientApp,
+export default function runUserApp(
+  app: UserApp,
   executors: Executor[]
 ): void {
   run((action$, { re }) => {
@@ -12,7 +12,7 @@ export default function runClient(
     const a$: O<A<any>> = executors
       .reduce((a$, { execute }) => a$.map(execute(options)), action$)
       .share();
-    const app$ = clientApp(a$, options);
+    const app$ = app(a$, options);
     executors.reduce((c, { after }) => after(c), options);
     return app$;
   });

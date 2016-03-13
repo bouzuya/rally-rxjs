703fcfe4f2301fd28c87deb6f8c78f0705e1d881
bouzuya <m@bouzuya.net>
2016-03-13 18:31:39 +0900

    bump boajs-router to 0.3.0

--- test/app/route/index.ts
+++ test/app/route/index.ts
@@ -3,32 +3,30 @@ import { routes } from '../../../src/app/route/';
 import { init, Router } from 'boajs-router';
 import { Route } from '../../../src/app/property-type/route';
 
-type MyRouter = Router<Route>;
-
 test.beforeEach(t => {
   t.context.router = init(routes);
 });
 
 test('sign_in#index', t => {
-  const router: MyRouter = t.context.router;
+  const router: Router = t.context.router;
   const path = '/sign_in';
   const match = router(path);
-  t.ok(match.route.name === 'sign_in#index');
+  t.ok(match.route['name'] === 'sign_in#index');
   t.same(match.params, {});
 });
 
 test('stamp_rallies#index', t => {
-  const router: MyRouter = t.context.router;
+  const router: Router = t.context.router;
   const path = '/stamp_rallies';
   const match = router(path);
-  t.ok(match.route.name === 'stamp_rallies#index');
+  t.ok(match.route['name'] === 'stamp_rallies#index');
   t.same(match.params, {});
 });
 
 test('stamp_rallies#show', t => {
-  const router: MyRouter = t.context.router;
+  const router: Router = t.context.router;
   const path = '/stamp_rallies/bouzuya';
   const match = router(path);
-  t.ok(match.route.name === 'stamp_rallies#show');
+  t.ok(match.route['name'] === 'stamp_rallies#show');
   t.same(match.params, <{ [k: string]: string; }>{ id: 'bouzuya' });
 });

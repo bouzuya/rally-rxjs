8ff0ca76426231026e5a6c10cd12868bcfc49183
bouzuya <m@bouzuya.net>
2016-03-18 18:18:06 +0900

    remove useless comments

--- src/app/request/index.ts
+++ src/app/request/index.ts
@@ -1,63 +1,19 @@
 import { Request } from 'boajs-handler-request';
 
-import requestSpotCreate from '../request/spot-create';
-import requestSpotIndex from '../request/spot-index';
-import requestStampRallyCreate from '../request/stamp-rally-create';
-import requestStampRallyIndex from '../request/stamp-rally-index';
-import requestStampRallyShow from '../request/stamp-rally-show';
-import requestTokenCreate from '../request/token-create';
-// import { create as responseSpotCreate } from '../action/response-spot-create';
-// import { create as responseSpotIndex } from '../action/response-spot-index';
-// import {
-//   create as responseStampRallyCreate
-// } from '../action/response-stamp-rally-create';
-// import {
-//   create as responseStampRallyIndex
-// } from '../action/response-stamp-rally-index';
-// import {
-//   create as responseStampRallyShow
-// } from '../action/response-stamp-rally-show';
-// import {
-//   create as responseTokenCreate
-// } from '../action/response-token-create';
+import spotCreate from '../request/spot-create';
+import spotIndex from '../request/spot-index';
+import stampRallyCreate from '../request/stamp-rally-create';
+import stampRallyIndex from '../request/stamp-rally-index';
+import stampRallyShow from '../request/stamp-rally-show';
+import tokenCreate from '../request/token-create';
 
 const requests: Request[] = [
-  {
-    name: 'spot-create',
-    request: requestSpotCreate
-    // ,
-    // responseToAction: responseSpotCreate
-  },
-  {
-    name: 'spot-index',
-    request: requestSpotIndex
-    // ,
-    // responseToAction: responseSpotIndex
-  },
-  {
-    name: 'stamp-rally-create',
-    request: requestStampRallyCreate
-    // ,
-    // responseToAction: responseStampRallyCreate
-  },
-  {
-    name: 'stamp-rally-index',
-    request: requestStampRallyIndex
-    // ,
-    // responseToAction: responseStampRallyIndex
-  },
-  {
-    name: 'stamp-rally-show',
-    request: requestStampRallyShow
-    // ,
-    // responseToAction: responseStampRallyShow
-  },
-  {
-    name: 'token-create',
-    request: requestTokenCreate
-    // ,
-    // responseToAction: responseTokenCreate
-  }
+  { name: 'spot-create', request: spotCreate },
+  { name: 'spot-index', request: spotIndex },
+  { name: 'stamp-rally-create', request: stampRallyCreate },
+  { name: 'stamp-rally-index', request: stampRallyIndex },
+  { name: 'stamp-rally-show', request: stampRallyShow },
+  { name: 'token-create', request: tokenCreate }
 ];
 
 export { requests };

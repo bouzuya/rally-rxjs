450f639cb31c5f4ebd941b290d54de7a318d0f2f
bouzuya <m@bouzuya.net>
2016-03-19 17:24:55 +0900

    add styles/views/sign-in-page

--- src/views/sign-in-page.ts
+++ src/views/sign-in-page.ts
@@ -12,27 +12,29 @@ import {
 const view = (state: State, helpers: any) => {
   const { create: h, e } = helpers;
   return h('div.sign-in-page', [
-    h('label', [
-      'email',
-      h('input.email', {
-        type: 'email',
-        name: 'email',
-        value: state.signIn.email,
-        onchange: ({ target: { value } }) => e(changeEmail(value))
-      }, []),
-    ]),
-    h('label', [
-      'password',
-      h('input.password', {
-        type: 'password',
-        name: 'password',
-        value: state.signIn.password,
-        onchange: ({ target: { value } }) => e(changePassword(value))
-      }, [])
-    ]),
-    h('button.sign-in', {
-      onclick: () => e(signIn())
-    }, ['sign in'])
+    h('form.sign-in-form', [
+      h('label.control.email', [
+        h('span.label', ['email']),
+        h('input.value', {
+          type: 'email',
+          name: 'email',
+          value: state.signIn.email,
+          onchange: ({ target: { value } }) => e(changeEmail(value))
+        }, []),
+      ]),
+      h('label.control.password', [
+        h('span.label', ['password']),
+        h('input.value', {
+          type: 'password',
+          name: 'password',
+          value: state.signIn.password,
+          onchange: ({ target: { value } }) => e(changePassword(value))
+        }, [])
+      ]),
+      h('button.sign-in-button', {
+        onclick: () => e(signIn())
+      }, ['sign in'])
+    ])
   ]);
 };
 
